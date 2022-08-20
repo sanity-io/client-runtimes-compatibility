e24dbe823f629de1ad51fbddca6ab188150adde6
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-20 17:44:43 +0200

    fix: test skunk

--- runtimes/edge-api-route/api/edge.ts
+++ runtimes/edge-api-route/api/edge.ts
@@ -1,11 +1,22 @@
+import createClient, { environment, condition } from 'skunkworks-test-client'
+
 export const config = {
   runtime: 'experimental-edge',
 }
 
+const projectId = '81pocpw8'
+const dataset = 'production'
+const apiVersion = 'v2021-03-25'
+const query = /* groq */ `count(*[studioVersion == 3])`
+
 export default async function handler(req) {
+  const client = createClient({ projectId, dataset, apiVersion, useCdn: true })
+  const result = await client.fetch(query)
   return new Response(
     JSON.stringify({
-      message: 'Hello, world!',
+      result,
+      environment,
+      condition,
     }),
     {
       status: 200,
--- skunkworks-test-client/lib/types.d.ts
+++ skunkworks-test-client/lib/types.d.ts
@@ -11,3 +11,6 @@ export default function createClient({
 }: ClientOptions): {
   fetch(query: string): Promise<any>
 }
+
+export const environment: string
+export const condition: string
