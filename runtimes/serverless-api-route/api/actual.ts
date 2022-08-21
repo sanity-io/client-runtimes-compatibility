376b7b9ac32031fd1cc0ecd9b7ebc1a7f27a29ea
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-21 19:13:02 +0200

    chore: use res

--- runtimes/serverless-api-route/api/actual.ts
+++ runtimes/serverless-api-route/api/actual.ts
@@ -1,14 +1,6 @@
-export default async function handler(req) {
-  return new Response(
-    JSON.stringify({
-      result: 'TODO',
-      'process.env.NEXT_RUNTIME': process.env.NEXT_RUNTIME,
-    }),
-    {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  )
+export default async function handler(req, res) {
+  return res.json({
+    result: 'TODO', 
+    'process.env.NEXT_RUNTIME': process.env.NEXT_RUNTIME,
+  })
 }
