a4d4d888ce8f666d3cf4186bb989e585cf75c83e
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-22 12:33:10 +0200

    fix: detect edge runtime

--- skunkworks-test-client/lib/env.cjs
+++ skunkworks-test-client/lib/env.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   document: typeof document === 'object',
+  EdgeRuntime: typeof EdgeRuntime === 'string' && EdgeRuntime,
   fetch: typeof fetch === 'function',
   global: typeof global === 'object',
   globalThis: typeof globalThis === 'object',
-  versions: typeof process.versions !== 'undefined' && process.versions,
   version: typeof process.version !== 'undefined' && process.version,
+  versions: typeof process.versions !== 'undefined' && process.versions,
   window: typeof window === 'object',
 }
