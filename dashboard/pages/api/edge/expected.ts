96292ef28dab7d2f5ba46edf3db5ee3dea57e0f5
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-22 15:12:43 +0200

    chore: add endpoints

--- dashboard/pages/api/edge/expected.ts
+++ dashboard/pages/api/edge/expected.ts
@@ -1,5 +1,5 @@
 import createClient, { target, condition } from 'skunkworks-test-client'
-import { projectId, dataset, apiVersion, query } from './_config'
+import { projectId, dataset, apiVersion, query } from '../_config'
 
 export const config = {
   runtime: 'experimental-edge',
--- dashboard/pages/api/nodejs/actual.ts
+++ dashboard/pages/api/nodejs/actual.ts
@@ -1,13 +1,14 @@
-import createClient, { target, condition } from 'skunkworks-test-client'
-import { projectId, dataset, apiVersion, query } from './_config'
+import createClient from '@sanity/client'
+import { projectId, dataset, apiVersion, query } from '../_config'
+
+export const config = {
+  runtime: 'nodejs',
+}
 
 export default async function handler(req, res) {
   const client = createClient({ projectId, dataset, apiVersion, useCdn: true })
   const result = await client.fetch(query)
   return res.json({
     result,
-    env: client.env,
-    target,
-    condition,
   })
 }
