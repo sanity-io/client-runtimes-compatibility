cfb96f52f4f40a9df18d66476064068b05cde586
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-22 14:01:58 +0200

    chore: test edge

--- runtimes/edge-api-route/api/actual.ts
+++ runtimes/edge-api-route/api/actual.ts
@@ -1,6 +1,4 @@
-// @TODO crashes on bundling step if importing up here
-// https://vercel.com/sanity-io/ecosystem-skunkworks-edge-api-route/FtoPZqXqgfMRS4sHmH8XbZzJKwhg
-// import createClient from '@sanity/client'
+import createClient from '@sanity/client'
 import { projectId, dataset, apiVersion, query } from './_config.mjs'
 
 export const config = {
@@ -8,22 +6,6 @@ export const config = {
 }
 
 export default async function handler() {
-  return new Response(
-    JSON.stringify({
-      error: `Build failed with 29 errors:
-vc-file-system:runtimes/edge-api-route/node_modules/@sanity/client/package.json:2:8: error: Expected ";" but found ":"
-vc-file-system:runtimes/edge-api-route/node_modules/core-util-is/lib/util.js:103:27: error: Could not resolve "buffer" (use "platform: 'node'" when building for node)
-vc-file-system:runtimes/edge-api-route/node_modules/decompress-response/index.js:2:41: error: Could not resolve "stream" (use "platform: 'node'" when building for node)
-vc-file-system:runtimes/edge-api-route/node_modules/decompress-response/index.js:3:21: error: Could not resolve "zlib" (use "platform: 'node'" when building for node)
-vc-file-system:runtimes/edge-api-route/node_modules/follow-redirects/index.js:1:18: error: Could not resolve "url" (use "platform: 'node'" when building for node)`,
-    }),
-    {
-      status: 500,
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  )
   try {
     const client = createClient({
       projectId,
