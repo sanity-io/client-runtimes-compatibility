31aa81d7d76efce73be19d44634e360cdcd8160f
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-22 12:26:20 +0200

    feat: more debug vars

--- skunkworks-test-client/lib/env.mjs
+++ skunkworks-test-client/lib/env.mjs
@@ -1,5 +1,9 @@
 export default {
   document: typeof document === 'object',
   fetch: typeof fetch === 'function',
+  global: typeof global === 'object',
+  globalThis: typeof globalThis === 'object',
+  versions: typeof process.versions !== 'undefined' && process.versions,
+  version: typeof process.version !== 'undefined' && process.version,
   window: typeof window === 'object',
 }
