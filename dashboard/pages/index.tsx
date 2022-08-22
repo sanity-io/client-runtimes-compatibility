ae01b89f37cb09b69ab31e316197920fb41e47c8
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-22 18:02:48 +0200

    chore: prettier

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -14,36 +14,44 @@ type Props = {
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 const useDashboardData = () => {
-  const { data, error } = useSWR('/data.json', fetcher, {revalidateOnMount: false})
+  const { data, error } = useSWR('/data.json', fetcher, {
+    revalidateOnMount: false,
+  })
   const [initial] = useState(() => stringify(data))
   const hash = useMemo(() => stringify(data), [data])
   const changed = initial !== hash
 
-  if(error) {
+  if (error) {
     throw error
   }
 
-  return {data, changed }
+  return { data, changed }
 }
 
 function Dashboard() {
-  const {data, changed} = useDashboardData()
-  return <>
-  Hello Hello
-  <pre>{JSON.stringify({data, changed}, null, 2)}</pre>
-  </>
+  const { data, changed } = useDashboardData()
+  return (
+    <>
+      Hello Hello
+      <pre>{JSON.stringify({ data, changed }, null, 2)}</pre>
+    </>
+  )
 }
 
-export default function IndexPage({fallback}: Props) {
-  return <SWRConfig value={{fallback}}><Dashboard /></SWRConfig>
+export default function IndexPage({ fallback }: Props) {
+  return (
+    <SWRConfig value={{ fallback }}>
+      <Dashboard />
+    </SWRConfig>
+  )
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   return {
     props: {
       fallback: {
-        '/data.json': data
-      }
-    }
+        '/data.json': data,
+      },
+    },
   }
 }
