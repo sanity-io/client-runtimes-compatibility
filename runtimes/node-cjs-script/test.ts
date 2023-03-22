43ec9bc5613c83131fa8e92cd8e54c6833cd3f0c
Cody Olsen <stipsan@gmail.com>
2023-03-22 15:06:27 +0100

    chore: test moduleResolution node16

--- runtimes/node-cjs-script/test.ts
+++ runtimes/node-cjs-script/test.ts
@@ -1,16 +1,18 @@
-'use strict'
 const writeFile = require('write-file-atomic')
 const { createClient } = require('@sanity/client')
+
 const projectId = '81pocpw8'
 const dataset = 'production'
 const apiVersion = 'v2021-03-25'
 const query = /* groq */ `count(*[studioVersion == 3])`
+
 async function expected() {
   const createClient = require('skunkworks-test-client')
   const conditions = require('skunkworks-test-client/supports-conditions')
   const { entry } = createClient
   const client = createClient({ projectId, dataset, apiVersion, useCdn: true })
   const result = await client.fetch(query)
+
   await writeFile(
     'artifacts/expected.json',
     JSON.stringify({ result, env: client.env, entry, conditions })
@@ -31,12 +33,16 @@ async function actual() {
     console.error(err)
     json = { error: err instanceof Error ? err.stack : err }
   }
+
   await writeFile('artifacts/actual.json', JSON.stringify(json))
 }
+
 async function main() {
   await Promise.all([expected(), actual()])
+
   return 0
 }
+
 main()
   .then(process.exit)
   .catch((err) => {
--- runtimes/node-esm-script/test.ts
+++ runtimes/node-esm-script/test.ts
@@ -1,16 +1,22 @@
-'use strict'
-const writeFile = require('write-file-atomic')
-const { createClient } = require('@sanity/client')
+import { writeFile } from 'node:fs/promises'
+import createClientExpected, { entry } from 'skunkworks-test-client'
+import conditions from 'skunkworks-test-client/supports-conditions'
+import { createClient as createClientActual } from '@sanity/client'
+
 const projectId = '81pocpw8'
 const dataset = 'production'
 const apiVersion = 'v2021-03-25'
 const query = /* groq */ `count(*[studioVersion == 3])`
+
 async function expected() {
-  const createClient = require('skunkworks-test-client')
-  const conditions = require('skunkworks-test-client/supports-conditions')
-  const { entry } = createClient
-  const client = createClient({ projectId, dataset, apiVersion, useCdn: true })
+  const client = createClientExpected({
+    projectId,
+    dataset,
+    apiVersion,
+    useCdn: true,
+  })
   const result = await client.fetch(query)
+
   await writeFile(
     'artifacts/expected.json',
     JSON.stringify({ result, env: client.env, entry, conditions })
@@ -19,7 +25,7 @@ async function expected() {
 async function actual() {
   let json
   try {
-    const client = createClient({
+    const client = createClientActual({
       projectId,
       dataset,
       apiVersion,
@@ -31,15 +37,8 @@ async function actual() {
     console.error(err)
     json = { error: err instanceof Error ? err.stack : err }
   }
+
   await writeFile('artifacts/actual.json', JSON.stringify(json))
 }
-async function main() {
-  await Promise.all([expected(), actual()])
-  return 0
-}
-main()
-  .then(process.exit)
-  .catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
+
+await Promise.all([expected(), actual()])
