20426626b59a721b31c7a7283eb353bdebf2bfdd
Cody Olsen <stipsan@gmail.com>
2023-03-28 20:40:30 +0200

    chore: bun report env

--- runtimes/bun-script/test.actual.js
+++ runtimes/bun-script/test.actual.js
@@ -1,5 +1,9 @@
 import { write } from 'bun'
-import { createClient } from '@sanity/client'
+import {
+  createClient,
+  unstable__adapter,
+  unstable__environment,
+} from '@sanity/client'
 
 import { projectId, dataset, apiVersion, query } from './config.js'
 
@@ -7,7 +11,7 @@ let json
 try {
   const client = createClient({ projectId, dataset, apiVersion, useCdn: true })
   const result = await client.fetch(query)
-  json = { result }
+  json = { result, unstable__adapter, unstable__environment }
 } catch (err) {
   console.error(err)
   json = { error: err.stack || err.toString() }
